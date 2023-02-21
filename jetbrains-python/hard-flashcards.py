5362c29b325ada3dd0a6eb921922d98fb286f069
geepum <geepum@outlook.kr>
2023-02-21 23:29:35 +0900

    Complete inheritance in python

--- jetbrains-python/hard-flashcards.py
+++ jetbrains-python/hard-flashcards.py
@@ -1,30 +1,19 @@
-def flash_card():
+class Animal:
+    def __init__(self, name):
+        self.name = name
     
-	num_of_cards = int(input('Input the number of cards:\n'))
-	base1 = 1
-	base2 = 0
-	lst_term = []
-	lst_definition = []
 
-	while base1 <= num_of_cards:
-		term = input(f'The term for card #{base1}:\n')
-		lst_term.append(term)
+class Bird(Animal):
+    def __repr__(self):
+        return self.name
+    
 
-		definition = input(f'The definition for card #{base1}:\n')
-		lst_definition.append(definition)
-
-		base1 += 1
-
-	while base2 < len(lst_term):
-
-		answer = input(f'Print the definition of "{lst_term[base2]}":\n')
-
-		if answer == lst_definition[base2]:
-			print('Correct!')
-		else:
-			print(f'Wrong. The right answer is "{lst_definition[base2]}".')
-
-		base2 += 1
+class Pigeon(Bird):
+    def __repr__(self):
+        return self.name
 
 
-flash_card()
+class Sparrow(Bird):
+    def __repr__(self):
+        return self.name
+    
