be6aaae28c30b759d27b6d57cbbf3e785b0d3f94
geepum <geepum@outlook.kr>
2023-02-21 21:16:52 +0900

    Complete stage 2/7

--- jetbrains-python/hard-flashcards.py
+++ jetbrains-python/hard-flashcards.py
@@ -1,4 +1,13 @@
-print("""Card:
-purchase
-Definition:
-buy""")
+def flash_card():
+	term = input()
+	definition = input()
+	answer = input()
+    
+
+	if definition == answer:
+		print('Your answer is right!')
+	else:
+		print('Your answer is wrong...')
+
+
+flash_card()
